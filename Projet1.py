01f3287a6f9732b623093ceb8647fee563709b4a
amelies08 <amelie.sarrazin2@gmail.com>
2026-09-09 13:39:35 -0400

    fonction

--- Projet1.py
+++ Projet1.py
@@ -1,29 +1,31 @@
 import mistletoe
 
-modifiedLines = []
 
-with open('Test.md', 'r') as markdownFile:
-    readFile = markdownFile.readlines()
-    for line in readFile:
-        modifiedLine = []
-        if line.find('/') != -1:
-            for letter in line:
-                modifiedLine.append(letter)
-            modifiedLine[0] = '<input type="checkbox"> <label>'
-            if line.find('\n') != 0:
-                modifiedLine[-1] = ('</label><br>')
+def checklistMD(nomFichier):
+    modifiedLines = []
+
+    with open(nomFichier + '.md', 'r') as markdownFile:
+        readFile = markdownFile.readlines()
+        for line in readFile:
+            modifiedLine = []
+            if line.find('/') != -1:
+                for letter in line:
+                    modifiedLine.append(letter)
+                modifiedLine[0] = '<input type="checkbox"> <label>'
+                if line.find('\n') != 0:
+                    modifiedLine[-1] = ('</label><br>')
+                else:
+                    modifiedLine.append('</label><br>')
+                modifiedLine.append('\n')
+                modifiedLines.append("".join(modifiedLine))
             else:
-                modifiedLine.append('</label><br>')
-            modifiedLine.append('\n')
-            modifiedLines.append("".join(modifiedLine))
-        else:
-            modifiedLines.append(line)
+                modifiedLines.append(line)
 
-with open('TestMD.md', 'w') as markdownFile:
-    markdownFile.write("".join(modifiedLines))
+    with open(nomFichier + 'MD.md', 'w') as markdownFile:
+        markdownFile.write("".join(modifiedLines))
 
-with open('TestMD.md', 'r') as fin:
-    rendered = mistletoe.markdown(fin)
+    with open(nomFichier + 'MD.md', 'r') as fin:
+        rendered = mistletoe.markdown(fin)
 
-with open('Test.html', 'w') as htmlFile:
-    htmlFile.write(rendered)
+    with open(nomFichier + '.html', 'w') as htmlFile:
+        htmlFile.write(rendered)
--- Test.py
+++
@@ -1,8 +0,0 @@
-with open('Test.md', 'r') as file:
-    for line in file:
-        if line == "/test":
-            modifiedLines = "TEST"
-            file.write(modifiedLines)
-
-#with open('Test.md', 'w') as file:
-#    file.write(modifiedLines)
