6224076ea7030cac411d44605b946bf742ca25ed
Amélie <amelie.sarrazin2@gmail.com>
2026-09-16 14:46:45 -0400

    Update Projet1.py

--- Projet1.py
+++ Projet1.py
@@ -1,5 +1,49 @@
 import mistletoe
 
+import mistletoe
+
+
+def convertir_diapositive(fichier_md, fichier_html):
+    """
+    Convertit un fichier Markdown contenant des 'Slide::'
+    en un fichier HTML contenant une div par diapositive.
+    """
+
+     # Ouvre le fichier Markdown en mode lecture
+    with open(fichier_md, 'r', encoding='utf-8') as contenu:
+         # Lit tout le contenu du fichier et le stock dans une variable
+        texte = contenu.read()
+
+    # Séparer les diapositives à chaque 'Slide::'
+    slides = texte.split("Slide::")
+
+    # CSS des diapositives, gestion de la couleur et de la taille de la diapositive
+    css = """
+    <style>
+        .slide {
+            background-color: rgb(158, 193, 230);
+            width: 100vw;
+            height: 70vh;
+        }
+    </style>
+    """
+    # Variable qui stock le contenu HTML 
+    resultat = ""
+
+    # Convertir chaque slide en HTML
+    for slide in slides:
+        rendu = mistletoe.markdown(slide)
+
+        # Place le contenu HTML dans une div ayant la classe "slide"
+        slide_html = '<div class="slide">' + rendu + '</div>'
+
+        # Ajoute la diapositive au résultat final
+        resultat += slide_html
+
+    # Écrire le résultat dans le fichier HTML
+    with open(fichier_html, 'w', encoding='utf-8') as fout:
+        # Écrit le CSS et toutes les diapositives dans le fichier HTML
+        fout.write(css + resultat)
 
 def checklistMD(fileName):
     """ This function changes the symbol / of a markdown file into a checkbox.
@@ -37,5 +81,9 @@ def checklistMD(fileName):
 
     with open(fileName + '.html', 'w') as htmlFile: # Open a new html file to write in it
         htmlFile.write(rendered) # Put the html string into a html file
+
+# Appelle la fonction avec le fichier Markdown en entrée
+# et le fichier HTML qui sera créé en sortie
+convertir_diapositive("TEST1.md", "TESTHTML.html")
 
 checklistMD("Test") # Exemple of the call of the function
